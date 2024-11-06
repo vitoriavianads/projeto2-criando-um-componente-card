f9d5b06eb8e2817d38db6c54b47d436438c655fd
Vitória Viana <159632505+vitoriavianads@users.noreply.github.com>
2024-11-06 07:53:51 -0300

    Add files via upload

---
+++ components/Card/index.jsx
@@ -0,0 +1,17 @@
+export default function Card() {
+    return (
+      <div>
+        <img src="./Card.jpg" alt="" />
+        <h1>Pôster: Star Wars (1977)</h1>
+        
+  
+        <p>Um pôster decorativo épico do filme Star Wars, com moldura de MDF e tamanho
+          A3. Uma ótima recordação de um dos mais icônicos filmes de todos
+          os tempos. Este clássico pôster trará aventura, nostalgia e a magia de Star Wars para qualquer lugar que 
+          você decidir pendurar. Não perca a chance de adicionaressa linda memória ao seu acervo!
+        </p>
+  
+        <button>Comprar agora</button>
+      </div>
+    )
+  }
